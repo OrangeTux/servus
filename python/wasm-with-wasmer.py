f95740f60f473e15dfe99292cc90da53bf4ec4cf
Auke Willem Oosterhoff <aukewillem.oosterhoff@mobilityhouse.com>
2020-01-21 09:54:05 +0100

    Use wasm-bindgen's allocator to allocate memory

--- python/wasm-with-wasmer.py
+++ python/wasm-with-wasmer.py
@@ -10,21 +10,19 @@ servus = Instance(wasm_bytes)
 sum = servus.exports.sum(3, 7)
 print(f'wasmer: sum of 3 and 7 is {sum}')
 
-# Offset in WASM memory where we start writing bytes.
-# If it is 0, 1, 2 or 3 it fails with:
-#
-#    RuntimeError: Call error: WebAssembly trap occured during runtime: memory out-of-bounds access
-# I've no clue yet why 4 is the magic number.
-OFFSET = 4
+data = b'OrangeTux'
 
-for i, c in enumerate(b'OrangeTux'):
-    servus.memory.uint8_view()[OFFSET + i] = c
+length = len(data)
 
-length = i + 1
+# Use the memory allocator from wasm-bindgen to allocate some memory.
+offset = servus.exports.__wbindgen_malloc(length)
+
+for i, c in enumerate(data):
+    servus.memory.uint8_view()[offset + i] = c
 
 # The second and third argument are respectively the offset in memory where
 # data starts and the length of the data
-servus.exports.reverse(8, OFFSET, length)
+servus.exports.reverse(8, offset, length)
 
 # The call to `reverse()` doesn't return a return value. Instead the offset and
 # and size of the data are written to index 2 and 3 of the 32 bit paged memory.
@@ -41,5 +39,4 @@ for i, c in enumerate(mem[offset:offset+length]):
     # WASM doesn't have a garbage collector, so we do that manually by erasing the memory.
     mem[i] = 0
 
-
-print(f"wasmer: the reverse of 'OrangeTux' is '{reverse}'")
+print(f"wasmer: the reverse of '{data}' is '{reverse}'")
